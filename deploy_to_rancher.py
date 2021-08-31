8662bc1d216855a2a99a4f3cd71460244fa2f34a
IvesCostaBr <ivespauiniam@gmail.com>
2021-08-31 10:20:13 -0500

    :hammer: fixing config deploy rancher

--- deploy_to_rancher.py
+++ deploy_to_rancher.py
@@ -1,20 +1,19 @@
 import os
 import sys
 import requests
-from requests.api import request
 
 class Log:
     def __init__(self, request):
         self.request = request
         
     def status_request(self):
-        if self.request.status_code > 200 and self.request.status_code < 299:
+        if self.request.status_code >= 200 and self.request.status_code < 299:
             print(f"""
-               \033[92m REPONSE --> {self.request.json()}  \033[0;0m
+               \033[92m REPONSE --> {self.request}  \033[0;0m
             """)
         else:
             print(f"""
-                \033[1;31m RESPONSE --> {self.request.json()} \033[0;0m
+                \033[1;31m RESPONSE FAIL--> {self.request} \033[0;0m
             """)
         
 
@@ -31,7 +30,7 @@ class DeployRancher:
         self.rancher_workload_url_api = ''
 
     def rancher_auth(self):
-        return requests.get('{}/projects/c-hmjq4:p-4xx87/workloads'.format(self.rancher_url_api), auth=(self.access_key, self.secret_key))
+        return requests.get('{}/projects'.format(self.rancher_url_api), auth=(self.access_key, self.secret_key))
 
     def deploy(self):
         rp = self.rancher_auth()
@@ -72,13 +71,18 @@ class DeployRancher:
                          json=response, auth=(self.access_key, self.secret_key))  
         Log(rget).status_request()
         sys.exit(0)
+        
+    def update_workload(self):
+        pass
+        
 
 
 def deploy_in_rancher(rancher_access_key, rancher_secret_key, rancher_url_api,
                       rancher_service_name, rancher_docker_image):
     deployment = DeployRancher(rancher_access_key, rancher_secret_key, rancher_url_api,
                                rancher_service_name, rancher_docker_image)
-    print(deployment.rancher_auth())
+      
+    deployment.deploy()
     return deployment
 
 
@@ -90,19 +94,42 @@ if __name__ == '__main__':
     rancher_docker_image = os.environ['DOCKER_IMAGE']
     rancher_docker_image_latest = os.environ['DOCKER_IMAGE_LATEST']
     
-    log = Log(DeployRancher(rancher_access_key, rancher_secret_key, rancher_url_api,
-             rancher_service_name, rancher_docker_image).rancher_auth())
+    # rancher_access_key = "token-twcj6"
+    # rancher_secret_key = "t5cdh4frvnplqlsndspwcs72pnp9qhfjw2c55mqkjbp9hvn27sq6js"
+    # rancher_url_api = "https://rancher.d3.do/v3"
+    # rancher_service_name = "d3-site-cms"
+    # rancher_docker_image = "929907635541.dkr.ecr.us-east-1.amazonaws.com/d3-site-cms:3f796bb665bafb66ca0532b39459b96ceaae796a"
+    # rancher_docker_image_latest = ""
+    # rancher_namespace_project = "d3-site-dev"
     
+    # log = Log(DeployRancher(rancher_access_key, rancher_secret_key, rancher_url_api,
+    #          rancher_service_name, rancher_docker_image).rancher_auth())
+    
+    # config = {
+    # "containers": [{
+    #     "imagePullPolicy": "IfNotPresent",
+    #     "image": rancher_docker_image,
+    #     "name": rancher_service_name,
+    # }],
+    # "namespaceId": "d3-site-dev",
+    # "name": rancher_service_name
+    # }
+    
+    # response = requests.post('{}/project/c-hmjq4:p-4xx87/workloads'.format(rancher_url_api), json=config, auth=(rancher_access_key, rancher_secret_key))
+    # print(response.json())
+    
+    # reponse = request.get('{}/')
+     
     try:
         deploy_in_rancher(rancher_access_key, rancher_secret_key, rancher_url_api,
                             rancher_service_name, rancher_docker_image)
         
+        
         if rancher_docker_image_latest != None and rancher_docker_image_latest != "":
             deploy_in_rancher(rancher_access_key, rancher_secret_key, rancher_url_api, 
                                 rancher_service_name, rancher_docker_image_latest)
-                 
+                    
     except KeyError:
-        log.status_request()
         sys.exit(1)
 
         
